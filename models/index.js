8fa10f05d650098666b82c60abf964f49ce20bfa
Betsy <betsy.griffith12@gmail.com>
2022-07-25 11:07:40 -0400

    added routes to get, post, put, and destroy user posts

--- models/index.js
+++ models/index.js
@@ -1,5 +1,14 @@
 const User = require("./User");
 const Post = require("./Post");
 
+// create associations between the user and the post and indicates that a user can have many posted articles
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+//tells which user the posted article belongs to
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
 
 module.exports = { User, Post };
