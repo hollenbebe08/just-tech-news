f1f84437dc43c4c554e90b60b3c53760c42817f4
Betsy <betsy.griffith12@gmail.com>
2022-07-25 11:56:29 -0400

    added ability to vote on posts and see what articles were voted on

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require("./User");
 const Post = require("./Post");
+const Vote = require('./Vote');
 
 // create associations between the user and the post and indicates that a user can have many posted articles
 User.hasMany(Post, {
@@ -11,4 +12,33 @@ Post.belongsTo(User, {
     foreignKey: 'user_id',
 });
 
-module.exports = { User, Post };
+//creates association between the article and the votes it receives
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+});
+  
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+});
+
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+  
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+  
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+  
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote };
