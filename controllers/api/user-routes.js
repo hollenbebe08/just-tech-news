ea1b55880e48c0adfb9d2aa7dc78c5eb855714b7
Betsy <betsy.griffith12@gmail.com>
2022-08-03 12:48:41 -0400

    created dashboard

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -70,49 +70,6 @@ router.post('/', (req, res) => {
     });
 });
 
-// router.post('/login', (req, res) => {
-    //expects {email: 'lernantino@gmail.com', password: 'password1234'}
-//     User.findOne({
-//         attributes: { exclude: ['password'] },
-//         where: {
-//           id: req.params.id
-//         },
-//         include: [
-//             {
-//               model: Post,
-//               attributes: ['id', 'title', 'post_url', 'created_at']
-//             },
-//             // include the Comment model here:
-//             {
-//               model: Comment,
-//               attributes: ['id', 'comment_text', 'created_at'],
-//               include: {
-//                 model: Post,
-//                 attributes: ['title']
-//               }
-//             },
-//             {
-//               model: Post,
-//               attributes: ['title'],
-//               through: Vote,
-//               as: 'voted_posts'
-//             }
-//         ]
-//     }).then(dbUserData => {
-//         if (!dbUserData) {
-//             res.status(400).json({message: 'No user with that email address!'});
-//             return;
-//         }
-//         //verify user
-//         const validPassword = dbUserData.checkPassword(req.body.password);
-//         if(!validPassword){
-//             res.status(400).json({ message: 'Incorrect password!'});
-//             return;
-//         }
-//         res.json({user: dbUserData, message: 'YOu are now logged in!'});
-//     });
-// });
-
 router.post('/login', (req, res) => {
     User.findOne({
       where: {
